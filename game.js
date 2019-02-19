b3650f7afa1b26c3c028ca8b8a8e027484454d52
Cerbi96 <lukaszgorecki96@gmail.com>
2019-02-19 19:28:27 +0100

    update cards, add cards hidden mechanism

--- game.js
+++ game.js
@@ -2,11 +2,29 @@ const cardsImage = ["ht","ht","cs","cs","js","js","ph","ph","jav","jav","pyth","
 let cards = document.querySelectorAll("div");
 cards = [...cards];
 
+const counter = new Date().getTime();
+
+const gamePairs = (cards.length)/2;
+let choosenCard = "";
+const pairOfCard = [];
+
+let gameResult = 0;
+
+const clickOnCard = function(){};
 const init = function(){
- cards.forEach(function(card){
+ cards.forEach(card => {
 const position = Math.floor(Math.random()*cardsImage.length);
 card.classList.add(cardsImage[position])
 cardsImage.splice(position,1);
  })   
+
+setTimeout(function(){
+    cards.forEach(card => {
+        card.classList.add("hidden")
+        card.addEventListener("click",clickOnCard)
+    })
+
+},3000)
 }
+
 init()
