02b57cef9d627157daef695c360bba8a77ccdd64
Cerbi96 <lukaszgorecki96@gmail.com>
2019-04-04 13:05:07 +0200

    show and hide cards update

--- game.js
+++ game.js
@@ -1,21 +1,42 @@
-const cardsImage = ["ht","ht","cs","cs","js","js","ph","ph","jav","jav","pyth","pyth","rea","rea","angu","angu","vju","vju"];
+const cardsColor = ["red","red","green","green","blue","blue","brown","brown","yellow","yellow","aqua","aqua","pink","pink","orange","orange","lightgreen","lightgreen"];
 let cards = document.querySelectorAll("div");
 cards = [...cards];
 
 const counter = new Date().getTime();
 
+
 const gamePairs = (cards.length)/2;
 let choosenCard = "";
-const pairOfCard = [];
+const pairOfCards = [];
 
 let gameResult = 0;
 
-const clickOnCard = function(){};
+
+const clickOnCard = function(){
+    choosenCard = this;
+    
+
+    if(choosenCard == pairOfCards[0])
+    {
+        return;
+    }
+    
+    choosenCard.classList.remove("hidden");
+    if(pairOfCards.length === 0){
+        console.log("1 element");
+        pairOfCards[0] = choosenCard;
+        return;
+    }else{
+        console.log("2 element");
+        cards.forEach(card => card.removeEventListener("click",clickOnCard))
+        pairOfCards[1] = choosenCard;
+    }
+}
 const init = function(){
  cards.forEach(card => {
-const position = Math.floor(Math.random()*cardsImage.length);
-card.classList.add(cardsImage[position])
-cardsImage.splice(position,1);
+const position = Math.floor(Math.random()*cardsColor.length);
+card.classList.add(cardsColor[position])
+cardsColor.splice(position,1);
  })   
 
 setTimeout(function(){
